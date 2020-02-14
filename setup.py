f60c670f5e54b3362ed6c95f925c453a192d0c13
MatheusAllein <mtsallein@gmail.com>
2020-02-14 10:18:42 -0300

    Initial release version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
   install_requires=[
     "requests",
   ],
-  version="1.0.0",
+  version="1.0.1",
   author="Matheus Allein",
   author_email="mtsallein@gmail.com",
   description="A collection of functions for fetching processes and products from DES.",
